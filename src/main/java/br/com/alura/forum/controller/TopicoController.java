2278e7e1dda7ded5e5866924391febb89681fc7b
Rodrigo Tenorio de BOa Ventura <boaventura19@yahoo.com.br>
2019-07-14 21:57:54 -0300

    Atualizacao da class TopicoController e criaçao do metodo check().

--- src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -2,11 +2,16 @@ package br.com.alura.forum.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +24,7 @@ import br.com.alura.forum.controller.dto.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
+import br.com.alura.forum.error.ResourceNotFoundException;
 
 @RestController
 @RequestMapping("/v1/topicos")
@@ -42,7 +48,14 @@ public class TopicoController {
 		return TopicoDTO.coverter(listTopico);
 	}
 	
+	@GetMapping(value="/{id}")
+	public TopicoDTO getById(@PathVariable Long id) {
+		Topico topico = topicoRepository.getOne(id);
+		return new TopicoDTO(topico);
+	}
+	
 	@PostMapping
+	@Transactional
 	public ResponseEntity<TopicoDTO> register(@RequestBody @Valid TopicoForm form,UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
@@ -51,6 +64,19 @@ public class TopicoController {
 		return ResponseEntity.created(uri).body(new TopicoDTO(topico));
 	}
 	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> delete(@PathVariable Long id){
+		check(id);
+		topicoRepository.deleteById(id);
+		return ResponseEntity.ok().build();
+	}
 	
+	private void check(Long id)  {
+	 Optional<Topico> topico=	topicoRepository.findById(id);
+	 if(! topico.isPresent()) 
+		 throw new ResourceNotFoundException("Topico Not Found for id " + id);
+	 
+	}
 
 }
--- src/main/java/br/com/alura/forum/controller/dto/TopicoDTO.java
+++ src/main/java/br/com/alura/forum/controller/dto/TopicoDTO.java
@@ -2,6 +2,7 @@ package br.com.alura.forum.controller.dto;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import br.com.alura.forum.modelo.Topico;
