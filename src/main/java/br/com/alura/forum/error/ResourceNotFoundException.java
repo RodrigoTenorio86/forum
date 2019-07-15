b6dbeccded1ab95e325e3a398223f0d2e08e222f
Rodrigo Tenório de Boa Ventura <boaventura19@yahoo.com.br>
2019-07-15 09:44:12 -0300

    Update ResourceNotFoundException.java
    incluir anotação "@SuppressWarnings("serial")" em class.

--- src/main/java/br/com/alura/forum/error/ResourceNotFoundException.java
+++ src/main/java/br/com/alura/forum/error/ResourceNotFoundException.java
@@ -3,6 +3,7 @@ package br.com.alura.forum.error;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@SuppressWarnings("serial")
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 	public ResourceNotFoundException(String message) {
